40c2d1fc8f9303636adb3ec5b9234d48f0f601ef
Bruno Rubiños <brunorubinosperez@gmail.com>
2023-06-04 11:26:30 -0500

    Se agrega movimiento al jugador y se define los limites de desplazamiento

--- main.js
+++ main.js
@@ -11,6 +11,11 @@ window.addEventListener("resize", setCanvasSize);
 let canvasSize;
 let elementsSize;
 
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
+
 function setCanvasSize() {
   window.innerHeight > window.innerWidth
     ? (canvasSize = window.innerWidth * 0.8)
@@ -25,16 +30,84 @@ function startGame() {
   context.font = elementsSize * 1 + "px verdana";
   context.textAlign = "end";
 
-  // const mapsRow = maps[0].trim().split("\n"); const map = mapsRow.map((row) => row.trim().split(""));
   const map = maps[0].match(/[IXO\-]+/g).map((row) => row.split(""));
 
-  context.clearRect(0,0,canvasSize, canvasSize);
+  context.clearRect(0, 0, canvasSize, canvasSize);
   map.forEach((row, rowIndex) => {
     row.forEach((col, colIndex) => {
       const emoji = emojis[col];
       const posX = elementsSize * (colIndex + 1 + 0.2);
       const posY = elementsSize * ((rowIndex + 1) * 0.98);
+      if (col === "O") {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posX * 0.94;
+          playerPosition.y = posY;
+        }
+      }
       context.fillText(emoji, posX, posY);
     });
   });
+
+  movePlayer();
+}
+
+function movePlayer() {
+  context.font = elementsSize * 0.9 + "px verdana";
+  context.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
+}
+
+const buttonUp = document.querySelector("#up");
+const buttonLeft = document.querySelector("#left");
+const buttonRight = document.querySelector("#right");
+const buttonDown = document.querySelector("#down");
+
+buttonUp.addEventListener("click", moveUp);
+buttonLeft.addEventListener("click", moveLeft);
+buttonRight.addEventListener("click", moveRight);
+buttonDown.addEventListener("click", moveDown);
+window.addEventListener("keydown", moveByKeys);
+
+function moveUp() {
+  console.log(playerPosition.y - elementsSize + ": " + elementsSize);
+  playerPosition.y - elementsSize < elementsSize
+    ? console.log("OUT")
+    : (playerPosition.y -= elementsSize * 0.975);
+  startGame();
+}
+function moveLeft() {
+  console.log(playerPosition.x - elementsSize + ": " + elementsSize);
+  playerPosition.x - elementsSize < elementsSize
+    ? console.log("OUT")
+    : (playerPosition.x -= elementsSize * 0.99);
+  startGame();
+}
+function moveRight() {
+  console.log(playerPosition.x + elementsSize + ": " + canvasSize);
+  playerPosition.x + elementsSize  > canvasSize + elementsSize
+    ? console.log("OUT")
+    : (playerPosition.x += elementsSize * 0.99);
+  startGame();
+}
+function moveDown() {
+  console.log(playerPosition.y + elementsSize + ": " + canvasSize);
+  playerPosition.y + elementsSize > canvasSize
+    ? console.log("OUT")
+    : (playerPosition.y += elementsSize * 0.975);
+  startGame();
+}
+function moveByKeys(event) {
+  switch (event.key) {
+    case "ArrowUp":
+      moveUp();
+      break;
+    case "ArrowLeft":
+      moveLeft();
+      break;
+    case "ArrowRight":
+      moveRight();
+      break;
+    case "ArrowDown":
+      moveDown();
+      break;
+  }
 }
